ba90db1a22a13f73540c208184ba87fd513872e3
simon <simon.tid@btinternet.com>
2023-01-03 22:07:23 +0000

    added allowed hosts

--- windward/settings.py
+++ windward/settings.py
@@ -26,9 +26,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
+
+ALLOWED_HOSTS = ['windward-paddocks.herokuapp.com', '127.0.0.1','www.windwardpaddocks.com']
 # Application definition
 
 INSTALLED_APPS = [
